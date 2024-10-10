c67faced165c3312f4eec70492cf67de8759459b
HLIB SIN <sinixyowo@gmail.com>
2024-10-10 03:57:27 +0300

    refact + reqs

--- sec/filing.py
+++ sec/filing.py
@@ -5,12 +5,12 @@ from .utils import cik_to_ticker, parse_title
 
 class Filing:
 
-    def __init__(self, cik: int, company: str, form: str, accession_number: str, updated: datetime):
+    def __init__(self, cik: int, company: str, form: str, accession_no: str, updated: datetime):
         self.cik = cik
         self.ticker = cik_to_ticker.get(self.cik, '')
         self.company = company
         self.form = form
-        self.accession_number = accession_number
+        self.accession_no = accession_no
         self.updated = updated
 
     def fits(self, filter: dict[str, list]) -> bool:
@@ -28,7 +28,7 @@ class Filing:
             'cik': self.cik,
             'company': self.company,
             'form': self.form,
-            'accession_number': self.accession_number,
+            'accession_no': self.accession_no,
             'updated': self.updated.isoformat(),
         }
 
@@ -36,5 +36,5 @@ class Filing:
     def from_xml_dict_entry(cls, entry: dict):
         title_dict = parse_title(entry['title'])
         updated = datetime.fromisoformat(entry['updated'])
-        accession_number = entry['id'].split('=')[-1]
-        return cls(**title_dict, accession_number=accession_number, updated=updated)
+        accession_no = entry['id'].split('=')[-1]
+        return cls(**title_dict, accession_no=accession_no, updated=updated)
--- sec/monitor.py
+++ sec/monitor.py
@@ -27,17 +27,17 @@ async def fetch_filings(page: int = 0) -> list[Filing]:
 async def monitor_filings(server: Server):
     def process_filings(filings: list[Filing], latest_an: str) -> bool:
         for f in filings:
-            if f.accession_number == latest_an: return True
+            if f.accession_no == latest_an: return True
             broadcast(f, server)
         return False
 
     filings = await fetch_filings(0)
-    latest_an = filings[-1].accession_number
+    latest_an = filings[-1].accession_no
     while True:
         t0 = time()
         page = 0
         filings = await fetch_filings(page)
-        new_latest_an = filings[0].accession_number
+        new_latest_an = filings[0].accession_no
         done = process_filings(filings, latest_an)
         while not done:
             if page == 9: raise Exception("That's too many pages; something gotta be wrong")
